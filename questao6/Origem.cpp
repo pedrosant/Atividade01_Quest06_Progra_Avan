034e80ae3db86af930515cf5048b5f1fc0e5b61a
pedrosant <roberto19santana@gmail.com>
2020-02-02 12:19:22 -0300

    Algumas otimizações

--- questao6/Origem.cpp
+++ questao6/Origem.cpp
@@ -5,16 +5,15 @@ using namespace std;
 int main() {
 	int num1 = 0; int num2 = 1; int num3; int aux; int n;
 	cout << "Digite o numero: ";
-	cin >> n;	
-		for (int i = 0; i < n; i++) {
-			num3 = num2 + num1;
-			if ( n >= 1) {
-				cout << num1 << ",";
-				aux = num2;
-				num2 = num3;
-				num1 = aux;
-				continue;
-			}			
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		num3 = num2 + num1;
+		if (n >= 1) {
+			cout << num1 << ",";
+			aux = num2;
+			num2 = num3;
+			num1 = aux;
+			continue;
 		}
-	return 0;
+	}
 }
